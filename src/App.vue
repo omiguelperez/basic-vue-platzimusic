8a3c21fbc8a66c30beb096262ac0632acd8dd2b8
Oscar Pérez <mr.omiguelperez@gmail.com>
2019-07-06 07:37:23 -0500

    Get top artists from last.fm

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,46 @@
 <template lang="pug">
   #app
     img(src='./assets/logo.png')
-    h1 {{ msg }}
-    h2 Essential Links
+    h1 Top Artists
     ul
-      li
-        a(href='https://vuejs.org', target='_blank') Core Docs
-      li
-        a(href='https://forum.vuejs.org', target='_blank') Forum
-      li
-        a(href='https://chat.vuejs.org', target='_blank') Community Chat
-      li
-        a(href='https://twitter.com/vuejs', target='_blank') Twitter
-    h2 Ecosystem
-    ul
-      li
-        a(href='http://router.vuejs.org/', target='_blank') vue-router
-      li
-        a(href='http://vuex.vuejs.org/', target='_blank') vuex
-      li
-        a(href='http://vue-loader.vuejs.org/', target='_blank') vue-loader
-      li
-        a(href='https://github.com/vuejs/awesome-vue', target='_blank') awesome-vue
+      li(v-for="artist in artists") {{ artist.name }}
 </template>
 
 <script>
+import { getArtists } from './api'
+
 export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      artists: []
     }
+  },
+  mounted: function () {
+    getArtists()
+      .then(artists => {
+        this.artists = artists
+      })
   }
 }
 </script>
 
 <style lang="stylus">
-  #app 
+  #app
     font-family 'Avenir', Helvetica, Arial, sans-serif
     -webkit-font-smoothing antialiased
     -moz-osx-font-smoothing grayscale
     text-align center
     color #2c3e50
     margin-top 60px
-  h1, h2 
+  h1, h2
     font-weight normal
-  ul 
+  ul
     list-style-type none
     padding 0
-  li 
+  li
     display inline-block
     margin 0 10px
-  a 
+  a
     color #42b983
 </style>
---
+++ src/api/config.js
@@ -0,0 +1,6 @@
+export default {
+  appname: 'platzimusic',
+  apiKey: '8810631b9d76c69a2762da8e2b99c1d2',
+  secret: 'aa991bb6541a1cb8a8aff71101bb603a',
+  registeredTo: 'omiguelperez'
+}
