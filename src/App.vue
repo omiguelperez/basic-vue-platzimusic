c4e42e8659fb093234b1448672b9f33f62c33254
Oscar Pérez <mr.omiguelperez@gmail.com>
2019-07-06 08:04:27 -0500

    List artists with component

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,11 @@
     img(src='./assets/logo.png')
     h1 Top Artists
     ul
-      li(v-for="artist in artists") {{ artist.name }}
+      Artist(v-for="artist in artists" :artist="artist" :key="artist.mbid")
 </template>
 
 <script>
+import Artist from './components/Artist.vue'
 import { getArtists } from './api'
 
 export default {
@@ -15,6 +16,9 @@ export default {
     return {
       artists: []
     }
+  },
+  components: {
+    Artist,
   },
   mounted: function () {
     getArtists()
